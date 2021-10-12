138a3c9242cd7213ba6ce3d1b125306d2b0b9b26
Alexandra Zontova <a.zontova@vironit.co.uk>
2021-10-12 13:31:35 +0300

    Initial Commit

---
+++ newsApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  newsApp
+//
+//  Created by User on 12.10.21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
