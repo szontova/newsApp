bcf79777e453c615aa4def5c7cfc554b1e896f69
Alexandra Zontova <a.zontova@vironit.co.uk>
2021-10-19 17:52:07 +0300

    add button

--- newsApp/Modules/SignIn/SignInViewController.swift
+++ newsApp/Modules/SignIn/SignInViewController.swift
@@ -8,9 +8,11 @@
 import UIKit
 
 class SignInViewController: UIViewController {
+    
     private var signInViewModel: SignInViewModel!
     private let titleLabel = UILabel()
     private let codeTextField = UITextField()
+    private let signInButton = UIButton(type: .system)
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -18,6 +20,7 @@ class SignInViewController: UIViewController {
         signInViewModel = SignInViewModel()
         createTitleLabel()
         createCodeTextField()
+        createSignInButton()
     }
     
     private func createTitleLabel() {
@@ -44,6 +47,22 @@ class SignInViewController: UIViewController {
         codeTextField.layer.borderWidth = 1
         codeTextField.layer.borderColor = #colorLiteral(red: 0.9339585077, green: 0.9601209991, blue: 1, alpha: 1)
         codeTextField.font = UIFont(name: "Dosis-Regular", size: 20)
+    }
+    
+    private func createSignInButton() {
+        let randomColor = [#colorLiteral(red: 0.4666666687, green: 0.7647058964, blue: 0.2666666806, alpha: 1), #colorLiteral(red: 0.9411764741, green: 0.4980392158, blue: 0.3529411852, alpha: 1), #colorLiteral(red: 0.9686274529, green: 0.78039217, blue: 0.3450980484, alpha: 1), #colorLiteral(red: 0.8078431487, green: 0.4261274117, blue: 0.4211240271, alpha: 1)].randomElement()
+        view.addSubview(signInButton)
+        signInButton.backgroundColor = randomColor
+        signInButton.setTitle("Sign In", for: .normal)
+        signInButton.snp.makeConstraints { maker in
+            maker.centerX.equalToSuperview()
+            maker.width.equalTo(200)
+            maker.top.equalTo(view.bounds.height * 0.7)
+            maker.height.equalTo(50)
+        }
+        signInButton.layer.cornerRadius = 20
+        signInButton.setTitleColor(.white, for: .normal)
+        signInButton.titleLabel?.font = UIFont(name: "Dosis-Regular", size: 24)
     }
     
     private func getDate() -> String {
