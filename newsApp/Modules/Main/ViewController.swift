23d2629a12b072ac240347cd85b335f1ca17645c
Alexandra Zontova <a.zontova@vironit.co.uk>
2021-10-19 12:15:52 +0300

    add animation in main screen

--- newsApp/Modules/Main/ViewController.swift
+++ newsApp/Modules/Main/ViewController.swift
@@ -5,12 +5,33 @@
 //  Created by User on 12.10.21.
 //
 
+import Lottie
+import SnapKit
 import UIKit
 
 class ViewController: UIViewController {
+    private let animationView = AnimationView()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = #colorLiteral(red: 0.01412864786, green: 0, blue: 0.04351914807, alpha: 1)
+        setupAnimationView()
+    }
+    
+    private func setupAnimationView() {
+        view.addSubview(animationView)
+        animationView.animation = Animation.named("loading")
+        animationView.contentMode = .scaleAspectFit
+        animationView.loopMode = .playOnce
+        animationView.animationSpeed = 0.9
+        animationView.play { _ in
+        }
+        
+        animationView.snp.makeConstraints { maker in
+            maker.height.equalTo(view.bounds.height * 0.6)
+            maker.width.equalTo(view.bounds.height * 0.6)
+            maker.centerX.equalToSuperview()
+            maker.centerY.equalToSuperview()
+        }
     }
 }
