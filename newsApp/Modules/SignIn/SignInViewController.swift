2245933cbdf14cbad085756cf20df39b8944e975
Alexandra Zontova <a.zontova@vironit.co.uk>
2021-10-19 18:02:37 +0300

    add animation in action of button

--- newsApp/Modules/SignIn/SignInViewController.swift
+++ newsApp/Modules/SignIn/SignInViewController.swift
@@ -5,6 +5,7 @@
 //  Created by User on 19.10.21.
 //
 
+import Lottie
 import UIKit
 
 class SignInViewController: UIViewController {
@@ -13,6 +14,8 @@ class SignInViewController: UIViewController {
     private let titleLabel = UILabel()
     private let codeTextField = UITextField()
     private let signInButton = UIButton(type: .system)
+    private let loadingView = UIView()
+    private let animationView = AnimationView()
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -50,7 +53,7 @@ class SignInViewController: UIViewController {
     }
     
     private func createSignInButton() {
-        let randomColor = [#colorLiteral(red: 0.4666666687, green: 0.7647058964, blue: 0.2666666806, alpha: 1), #colorLiteral(red: 0.9411764741, green: 0.4980392158, blue: 0.3529411852, alpha: 1), #colorLiteral(red: 0.9686274529, green: 0.78039217, blue: 0.3450980484, alpha: 1), #colorLiteral(red: 0.8078431487, green: 0.4261274117, blue: 0.4211240271, alpha: 1)].randomElement()
+        let randomColor = [#colorLiteral(red: 0.4666666687, green: 0.7647058964, blue: 0.2666666806, alpha: 1), #colorLiteral(red: 0.9411764741, green: 0.4980392158, blue: 0.3529411852, alpha: 1), #colorLiteral(red: 0.9686274529, green: 0.78039217, blue: 0.3450980484, alpha: 1), #colorLiteral(red: 0.8078431487, green: 0.3853882797, blue: 0.4252633374, alpha: 1)].randomElement()
         view.addSubview(signInButton)
         signInButton.backgroundColor = randomColor
         signInButton.setTitle("Sign In", for: .normal)
@@ -63,6 +66,38 @@ class SignInViewController: UIViewController {
         signInButton.layer.cornerRadius = 20
         signInButton.setTitleColor(.white, for: .normal)
         signInButton.titleLabel?.font = UIFont(name: "Dosis-Regular", size: 24)
+        
+        signInButton.addTarget(self, action: #selector(tappedSignInButton), for: .touchUpInside)
+    }
+    
+    private func createLoadingView() {
+        view.addSubview(loadingView)
+        loadingView.backgroundColor = #colorLiteral(red: 0.01412864786, green: 0, blue: 0.04351914807, alpha: 0.5)
+        loadingView.snp.makeConstraints { maker in
+            maker.width.equalToSuperview()
+            maker.height.equalToSuperview()
+        }
+        createAnimationView()
+    }
+    
+    private func createAnimationView() {
+        loadingView.addSubview(animationView)
+        animationView.animation = Animation.named("loading")
+        animationView.contentMode = .scaleAspectFit
+        animationView.loopMode = .playOnce
+        animationView.animationSpeed = 0.9
+        animationView.play { _ in
+            UIView.transition(with: self.view, duration: 0.4, options: .transitionCrossDissolve, animations: {
+                self.loadingView.removeFromSuperview()
+            })
+        }
+        
+        animationView.snp.makeConstraints { maker in
+            maker.height.equalTo(view.bounds.height * 0.5)
+            maker.width.equalTo(view.bounds.height * 0.5)
+            maker.centerX.equalToSuperview()
+            maker.centerY.equalToSuperview()
+        }
     }
     
     private func getDate() -> String {
@@ -72,5 +107,16 @@ class SignInViewController: UIViewController {
         dateFormatter.dateFormat = "dd-MM-yyyy HH:mm:ss"
         dateFormatter.timeZone = calendar.timeZone
         return dateFormatter.string(from:currentDate)
+    }
+    
+    private func getUser() -> String {
+        return ""
+    }
+    
+    // MARK: - Actions
+    @objc private func tappedSignInButton() {
+        UIView.transition(with: view, duration: 0.4, options: .transitionCrossDissolve, animations: {
+            self.createLoadingView()
+        })
     }
 }
