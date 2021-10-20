8965fbb4a246480d1d0fb058634b3b841f645965
Alexandra Zontova <a.zontova@vironit.co.uk>
2021-10-20 09:44:41 +0300

    create APIConstants

---
+++ newsApp/Utils/APIConstants.swift
@@ -0,0 +1,16 @@
+//
+//  APIConstants.swift
+//  newsApp
+//
+//  Created by User on 20.10.21.
+//
+
+import Foundation
+
+struct APIConstants {
+    static let baseURL = "https://newsapi.org/v2/top-headlines"
+    static let nameSources = "sources"
+    static let valueSources = "bbc-news"
+    static let nameKey = "apiKey"
+    static let valueKey = "4812833d0d0345e4be9d3fd2efb9ff84"
+}
